38ca05532b0a0be7691084dc52388b4cf2736a18
Max15-cyber <max80021151@gmail.com>
2022-04-03 16:33:14 +0300

    normalized numbers

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,78 +1,37 @@
 const Game = () => {
     let data = [
         [1, 2, 3, 4, 5, 6],
+        [1, 2, 0, 4, 5, 6],
+        [1, 2, 3, 4, 5, 0],
+        [0, 2, 3, 4, 5, 6],
         [1, 2, 3, 4, 5, 6],
-        [1, 2, 3, 4, 5, 6],
-        [1, 2, 3, 4, 5, 6],
-        [1, 2, 3, 4, 5, 6],
-        [1, 2, 3, 4, 5, 6]
+        [1, 2, 3, 4, 0, 6]
     ]
-    // const tbody = 
+    let numbers = [1, 2, 3, 4, 5, 6];
+    let errors = 0;
+    const tbody = data.map((tr, trIndex) => {
+        return (
+            <tr key={trIndex}>{tr.map((td, tdIndex) => {
+                return (
+                    <td key={tdIndex}>{td || ''}</td>
+                )
+            })}</tr>
+        )
+    });
+    const values = numbers.map((number) => {
+        return(
+            <li className="number">{number}</li>
+        )
+    })
     return (
         <div className='container'>
-        <h1>Sudoku</h1>
-        <table className="table">
-          <tbody>
-            <tr className="tr">
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-        </table>
-        <ul className="numbers">
-          <li className="number">1</li>
-          <li className="number">2</li>
-          <li className="number">3</li>
-          <li className="number">4</li>
-          <li className="number">5</li>
-          <li className="number">6</li>
-        </ul>
-        <div className="errors">Errors: 0 / 3</div>
-      </div>
+            <h1>Sudoku</h1>
+            <table className="table">
+                <tbody>{tbody}</tbody>
+            </table>
+            <ul className="numbers"></ul>
+            <div className="errors">Errors: {errors} / 3</div>
+        </div>
     )
 }
 
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,8 +5,6 @@ const Menu = () => {
         <div className='container'>
             <h1 className='page-name'>Sudoku</h1>
             <NavLink className='btn' to='/game'>Start Game</NavLink>
-            <NavLink className='btn' to='/game_over'>End Game</NavLink>
-            <NavLink className='btn' to='/win'>Skip Mission</NavLink>
         </div>
     )
 }
